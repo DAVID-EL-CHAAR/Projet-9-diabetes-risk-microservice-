2732e1d5fa5ea5a533acfb364ff1840b9c5779b6
David El chaar <hamoudec@gmail.com>
2024-10-10 05:37:16 +0200

    new modif

--- src/main/java/com/diabetes_risk/repository/NoteRepository.java
+++ src/main/java/com/diabetes_risk/repository/NoteRepository.java
@@ -1,6 +1,7 @@
 package com.diabetes_risk.repository;
 
 import com.diabetes_risk.model.Note;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,12 +13,15 @@ public class NoteRepository {
 
     private final RestTemplate restTemplate;
 
+    @Value("${microservice.note-service-url}")
+    private String noteServiceUrl;
+
     public NoteRepository(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public List<Note> getNotesByPatientId(String patientId) {
-        String url = "http://localhost:8083/notes/patient/" + patientId;  // Appel au microservice Note
+        String url = noteServiceUrl + "/patient/" + patientId;  // Appel au microservice Note avec l'URL variabilisée
         Note[] notesArray = restTemplate.getForObject(url, Note[].class);
         return Arrays.asList(notesArray);
     }
--- src/main/java/com/diabetes_risk/repository/PatientRepository.java
+++ src/main/java/com/diabetes_risk/repository/PatientRepository.java
@@ -1,6 +1,7 @@
 package com.diabetes_risk.repository;
 
 import com.diabetes_risk.model.Patient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,12 +10,15 @@ public class PatientRepository {
 
     private final RestTemplate restTemplate;
 
+    @Value("${microservice.patient-service-url}")
+    private String patientServiceUrl;
+
     public PatientRepository(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     public Patient getPatientById(Long patientId) {
-        String url = "http://localhost:8080/patients/" + patientId;  // Appel au microservice Patient
+        String url = patientServiceUrl + "/" + patientId;  // Appel au microservice Patient avec l'URL variabilisée
         return restTemplate.getForObject(url, Patient.class);
     }
 }
